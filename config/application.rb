a53f357e3eba48b33c63315c6316f0de502d416b
Artty <qwertty2147@gmail.com>
2019-04-23 17:01:49 +0700

    fix app commit

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,8 @@
+
 require_relative 'boot'
-
+RAILS_ENV=production bundle exec rake assets:precompile
 require 'rails/all'
-
+config.assets.initialize_on_precompile = false
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,7 +11,7 @@ module Myblog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-	config.assets.initialize_on_precompile = false
+	
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
